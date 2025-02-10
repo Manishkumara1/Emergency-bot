73707a8db3605904051393ada1ed7e4ab2df124f
Manishkumara1 <Mk0dehura@gmail.com>
2025-02-10 08:19:02 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "21537501"
 api_hash = "02d8ef0eae2926ec4fd0cbff05ec0737"
-bot_token = "6940385889:AAF8jImlGxKw9VFGlYEuD6EaSQEKqWk7e10"
+bot_token = "7013832215:AAGUP5D7APrtiqpWy5d4VCsGMsnvFNSSDBA"
